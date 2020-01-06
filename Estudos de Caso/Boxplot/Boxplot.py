b24dbdc74ba6e2d35c98fd32c5b5dfd47a1d0601
Sandro Lucas Azevedo Ferreira <sandro.ferreira@agibank.com.br>
2020-01-06 13:42:55 -0300

    conclusão do curso

---
+++ Estudos de Caso/Boxplot/Boxplot.py
@@ -0,0 +1,13 @@
+# Boxplot - Diagrama de caixa
+import matplotlib.pyplot as plt
+import random
+
+vetor = []
+
+for i in range(10):
+    numero_aleatorio = random.randint(0, 100)
+    vetor.append(numero_aleatorio)
+
+plt.boxplot(vetor)
+plt.title("Boxplot")
+plt.show()
